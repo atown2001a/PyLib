b303344d65251417ae2e2892890b23b8a0bda91f
Ahmed Al-Meleh <aalmeleh.whatever.0101@gmail.com>
2026-01-22 21:08:02 +0000

    Added new themes to try for better UI/UX

---
+++ static/script.js
@@ -0,0 +1,145 @@
+$('#btnC1').on('click', function () {
+	$('body').css('background', 'black');
+        $('h1').css('color', 'white');
+        $('h2').css('color', 'white');
+        $('h3').css('color', 'white');
+        $('h4').css('color', 'white');
+        $('p').css('color', 'white');
+        $('li').css('color', 'white');
+	$('table').css('color', 'white');
+        $('ul').css('background', 'darkblue');
+        $('#active').css('background', 'maroon')
+        $('#contentX').css('background', 'maroon');
+	$('#lBTN').css('background', 'darkgreen');
+        });
+function fBorder() {
+   var element = document.getElementById("contentX");
+   element.classList.add("bor1");
+}
+
+$('#btnC2').on('click', function () {
+	$('body').css('background', 'black');
+        $('h1').css('color', 'white');
+        $('h2').css('color', 'white');
+        $('h3').css('color', 'white');
+        $('h4').css('color', 'white');
+        $('p').css('color', 'white');
+        $('li').css('color', 'white');
+	$('table').css('color', 'white');
+        $('ul').css('background', 'darkblue');
+        $('#active').css('background', 'darkgreen')
+        $('#contentX').css('background', 'darkgreen');
+	$('#lBTN').css('background', 'maroon');
+        });
+function fBorder2() {
+   var element = document.getElementById("contentX");
+   element.classList.add("bor1");
+}
+
+
+$('#btnC3').on('click', function () {
+	$('body').css('background', 'black');
+        $('h1').css('color', 'white');
+        $('h2').css('color', 'white');
+        $('h3').css('color', 'white');
+        $('h4').css('color', 'white');
+        $('p').css('color', 'white');
+        $('li').css('color', 'white');
+        $('table').css('color', 'white');
+        $('ul').css('background', 'darkgreen');
+        $('#active').css('background', 'maroon')
+        $('#contentX').css('background', 'maroon');
+	$('#lBTN').css('background', 'darkblue');
+        });
+function fBorder3() {
+   var element = document.getElementById("contentX");
+   element.classList.add("bor1");
+}
+
+$('#btnC4').on('click', function () {
+	$('body').css('background', 'black');
+        $('h1').css('color', 'white');
+        $('h2').css('color', 'white');
+        $('h3').css('color', 'white');
+        $('h4').css('color', 'white');
+        $('p').css('color', 'white');
+        $('li').css('color', 'white');
+	$('table').css('color', 'white');
+        $('ul').css('background', 'darkgreen');
+        $('#active').css('background', 'darkblue')
+        $('#contentX').css('background', 'darkblue');
+	$('#lBTN').css('background', 'maroon');
+        });
+function fBorder4() {
+   var element = document.getElementById("contentX");
+   element.classList.add("bor1");
+}
+
+
+
+$('#btnC5').on('click', function () {
+	$('body').css('background', 'black');
+        $('h1').css('color', 'white');
+        $('h2').css('color', 'white');
+        $('h3').css('color', 'white');
+        $('h4').css('color', 'white');
+        $('p').css('color', 'white');
+        $('li').css('color', 'white');
+	$('table').css('color', 'white');
+        $('ul').css('background', 'maroon');
+        $('#active').css('background', 'darkgreen')
+        $('#contentX').css('background', 'darkgreen');
+	$('#lBTN').css('background', 'darkblue');
+        });
+function fBorder5() {
+   var element = document.getElementById("contentX");
+   element.classList.add("bor1");
+}
+
+$('#btnC6').on('click', function () {
+	$('body').css('background', 'black');
+        $('h1').css('color', 'white');
+        $('h2').css('color', 'white');
+        $('h3').css('color', 'white');
+        $('h4').css('color', 'white');
+        $('p').css('color', 'white');
+        $('li').css('color', 'white');
+	$('table').css('color', 'white');
+        $('ul').css('background', 'maroon');
+        $('#active').css('background', 'darkblue')
+        $('#contentX').css('background', 'darkblue');
+	$('#lBTN').css('background', 'darkgreen');
+        });
+function fBorder6() {
+   var element = document.getElementById("contentX");
+   element.classList.add("bor1");
+}
+
+function fBlack() {
+//Black
+  document.getElementById("lBTN").style.backgroundColor = "black";
+}
+function fGreen() {
+//Green
+  document.getElementById("lBTN").style.backgroundColor = "#008000";
+}
+function fRed() {
+//Red
+  document.getElementById("lBTN").style.backgroundColor = "#800000";
+}
+function fBlue() {
+//Blue
+  document.getElementById("lBTN").style.backgroundColor = "blue";
+}
+function fDarkBlue() {
+//dblue
+  document.getElementById("lBTN").style.backgroundColor = "darkblue";
+}
+function fGrey() {
+//Grey
+  document.getElementById("lBTN").style.backgroundColor = "darkslategrey";
+}
+
+function GoBack() {
+location.reload(); 
+}
